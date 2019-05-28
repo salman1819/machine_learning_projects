effc0b90e446d2d27c6fc20fffaafef0985cee7f
Salman Shaikh <salmanshanu99@gmail.com>
2019-05-28 14:12:27 -0700

    Add files via upload

---
+++ mask_r_cnn_keras.py
@@ -0,0 +1,92 @@
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+
+# draw an image with detected objects
+def draw_image_with_boxes(filename, boxes_list):
+     # load the image
+     data = pyplot.imread(filename)
+     # plot the image
+     pyplot.imshow(data)
+     # get the context for drawing boxes
+     ax = pyplot.gca()
+     # plot each box
+     for box in boxes_list:
+          # get coordinates
+          y1, x1, y2, x2 = box
+          # calculate width and height of the box
+          width, height = x2 - x1, y2 - y1
+          # create the shape
+          rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+          # draw the box
+          ax.add_patch(rect)
+     # show the plot
+     pyplot.show()
+
+# define the test configuration
+class TestConfig(Config):
+     NAME = "test"
+     GPU_COUNT = 1
+     IMAGES_PER_GPU = 1
+     NUM_CLASSES = 1 + 80
+
+# define the model
+rcnn = MaskRCNN(mode='inference', model_dir='./', config=TestConfig())
+# load coco model weights
+rcnn.load_weights('mask_rcnn_coco.h5', by_name=True)
+# load photograph
+img = load_img('elephant.jpg')
+img = img_to_array(img)
+# make prediction
+results = rcnn.detect([img], verbose=0)
+# visualize the results
+draw_image_with_boxes('elephant.jpg', results[0]['rois'])
+
+
+# example of inference with a pre-trained coco model
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from mrcnn.visualize import display_instances
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+
+# define 81 classes that the coco model knowns about
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+# define the test configuration
+class TestConfig(Config):
+     NAME = "test"
+     GPU_COUNT = 1
+     IMAGES_PER_GPU = 1
+     NUM_CLASSES = 1 + 80
+
+# define the model
+rcnn = MaskRCNN(mode='inference', model_dir='./', config=TestConfig())
+# load coco model weights
+rcnn.load_weights('mask_rcnn_coco.h5', by_name=True)
+# load photograph
+img = load_img('elephant.jpg')
+img = img_to_array(img)
+# make prediction
+results = rcnn.detect([img], verbose=0)
+# get dictionary for first prediction
+r = results[0]
+# show photo with bounding boxes, masks, class labels and scores
+display_instances(img, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
